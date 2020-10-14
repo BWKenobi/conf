0f47584e383abde624db9803a5a860c8e8a07312
bwkenobi <boriszhuravlev@gmail.com>
2020-10-14 11:03:20 +0300

    Add full registration and reset password

--- profileuser/models.py
+++ profileuser/models.py
@@ -16,7 +16,7 @@ class Profile(models.Model):
 	surname = models.CharField(verbose_name="Фамилия*", max_length=50, blank=True)
 	name = models.CharField(verbose_name="Имя*", max_length=30, blank=True)
 	name2 = models.CharField(verbose_name="Отчество*", max_length=30, blank=True)
-	work_place = models.TextField(verbose_name="Место работы (полностью)*", max_length=30, blank=True)
+	work_place = models.TextField(verbose_name="Место работы (полностью)*", blank=True)
 
 	registration_date = models.DateField(verbose_name="Дата регистрации", default=timezone.now)
 	admin_access= models.BooleanField("Права администратора", default=False)
