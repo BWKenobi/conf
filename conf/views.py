7dfd69824ef0c06b36e64cab0ba33b3e41ff859b
bwkenobi <boriszhuravlev@gmail.com>
2020-11-07 14:51:02 +0300

    Add making sertificates

--- conf/views.py
+++ conf/views.py
@@ -123,8 +123,7 @@ def home_view(request):
 
 		response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
 		response['Content-Disposition'] = 'attachment; filename=List (' + dte.strftime('%d-%b-%Y') + ').docx'
-		#document.save(response)
-		document.save(os.path.join(settings.MEDIA_ROOT, '111.docx'))
+		document.save(response)
 
 		return response
 
